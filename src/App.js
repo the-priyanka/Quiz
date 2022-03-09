1e1a762db15f224ab5983bdc9635571f2ee5125b
Priyanka <original.manaria@gmail.com>
2022-03-09 12:40:46 +0530

    display questions

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useGlobalContext } from "./context";
 
 import SetupForm from "./components/SetupForm";
 import Loading from "./components/Loading";
+import Modal from "./components/Modal";
 
 function App() {
   const { waiting, loading, questions, index, correct } =
@@ -14,7 +15,33 @@ function App() {
     return <Loading />;
   }
 
-  return <main>the priyanka</main>;
+  const { question, incorrect_answers, correct_answer } =
+    questions[0];
+  const answers = [...incorrect_answers, correct_answer];
+
+  return (
+    <main>
+      {/* <Modal /> */}
+      <section className="quiz">
+        <p className="correct-answers">
+          correct answers : {correct} / {index}
+        </p>
+        <article className="container">
+          <h2 dangerouslySetInnerHTML={{ __html: question }} />
+          {answers.map((answer, index) => {
+            return (
+              <button
+                key={index}
+                className="answer-btn"
+                dangerouslySetInnerHTML={{ __html: answer }}
+              />
+            );
+          })}
+        </article>
+        <button className="next-question">next question</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
