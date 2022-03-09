9d59308b18668bfa5872b77a071d09f97d521612
Priyanka <original.manaria@gmail.com>
2022-03-09 17:19:42 +0530

    handle form structure

--- src/context.js
+++ src/context.js
@@ -13,6 +13,11 @@ const AppProvider = (props) => {
   const [index, setIndex] = useState(0);
   const [correct, setCorrect] = useState(0);
   const [error, setError] = useState(false);
+  const [quiz, setQuiz] = useState({
+    amount: 10,
+    category: "sports",
+    difficulty: "easy",
+  });
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const fetchQuestions = async (url) => {
@@ -66,9 +71,13 @@ const AppProvider = (props) => {
     setIsModalOpen(false);
   };
 
-  useEffect(() => {
-    fetchQuestions(tempUrl);
-  }, []);
+  const handleChange = (e) => {
+    console.log(e);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <AppContext.Provider
@@ -84,6 +93,9 @@ const AppProvider = (props) => {
         checkAnswer,
         openModal,
         closeModal,
+        handleChange,
+        handleSubmit,
+        quiz,
       }}
     >
       {props.children}
