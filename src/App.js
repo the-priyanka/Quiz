e8b3a3668dea64d774234b59270cbb596a39c645
Priyanka <original.manaria@gmail.com>
2022-03-09 18:10:51 +0530

    random current answers

--- src/App.js
+++ src/App.js
@@ -24,7 +24,15 @@ function App() {
 
   const { question, incorrect_answers, correct_answer } =
     questions[index];
-  const answers = [...incorrect_answers, correct_answer];
+  // const answers = [...incorrect_answers, correct_answer];
+  const answers = [...incorrect_answers];
+  const tempIndex = Math.floor(Math.random() * 4);
+  if (tempIndex === 3) {
+    answers.push(correct_answer);
+  } else {
+    answers.push(answers[tempIndex]);
+    answers[tempIndex] = correct_answer;
+  }
 
   return (
     <main>
